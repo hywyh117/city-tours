51a0a010e2a29c69f8b0919226ad7d992e78d0fa
Max <max@acnem.org>
2020-06-19 17:51:43 +1000

    complete tour component content

--- src/Components/Tour/Tour.js
+++ src/Components/Tour/Tour.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
+import "./tour.scss";
 
 export default class Tour extends Component {
   render() {
-    return <div>hello from Tour</div>;
+    console.log(this.props);
+    const { id, city, img, name, info } = this.props.tour;
+    const { removeTour } = this.props;
+    // console.log(this.props.tour);
+    return (
+      <article className="tour">
+        <div className="img-container">
+          <img src={img} alt="img of the city" />
+          <span className="close-btn">
+            <i className="fas fa-window-close"></i>
+          </span>
+        </div>
+        <div className="tour-info">
+          <h3>{city}</h3>
+          <h4>{name}</h4>
+          <h5>
+            info <i className="fas fa-caret-square-down"></i>
+          </h5>
+          <p>{info}</p>
+        </div>
+      </article>
+    );
   }
 }
