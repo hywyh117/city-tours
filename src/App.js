08ec8313ffce47df25de35bf3108ee0751b6d512
Max <max@acnem.org>
2020-06-19 23:47:13 +1000

    remove unused vars

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.scss";
 import Navbar from "./Components/Nevbar/Navbar";
 import TourList from "./Components/TourList";
-import Tour from "./Components/Tour/Tour";
+// import Tour from "./Components/Tour/Tour";
 
 class App extends Component {
   render() {
